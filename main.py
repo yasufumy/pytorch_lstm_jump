f54eee0529acdfec7b2aef2e06af31104bfedb54
yasufumi <yasufumi.taniguchi@gmail.com>
2018-03-08 14:39:48 +0900

    update main.py

--- main.py
+++ main.py
@@ -5,34 +5,66 @@ from torch import optim
 
 from torchtext import data
 from torchtext import datasets
+from torchtext.vocab import FastText
 
 from model import LSTMJump
+
+
+PAD_TOKEN = '<pad>'
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
+    parser.add_argument('--batch', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--R', type=int, default=20)
     parser.add_argument('--K', type=int, default=40)
     parser.add_argument('--N', type=int, default=5)
     return parser.parse_args()
 
 
+def pick_fix_length(length):
+    import random
+
+    def _pick(arr, vocab, train):
+        if train:
+            max_length = len(arr[0])
+            pad_id = vocab.stoi[PAD_TOKEN]
+            if max_length > length:
+                result = []
+                for ex in arr:
+                    real_length = len([x for x in ex if x != pad_id])
+                    if real_length > length:
+                        n = random.randrange(real_length - length + 1)
+                        result.append(ex[n:n + length])
+                    else:
+                        result.append(ex[:length])
+                return result
+            else:
+                return [ex + [pad_id] * (length - max_length) for ex in arr]
+        else:
+            return arr
+    return _pick
+
+
 def main(args):
-    TEXT = data.Field(lower=True)
-    LABEL = data.Field(sequential=False)
+    TEXT = data.Field(lower=True, postprocessing=pick_fix_length(400), pad_token=PAD_TOKEN)
+    LABEL = data.Field(sequential=False, pad_token=None, unk_token=None)
 
     train, test = datasets.IMDB.splits(TEXT, LABEL)
 
-    TEXT.build_vocab(train)
+    TEXT.build_vocab(train, vectors=FastText())
     LABEL.build_vocab(train)
 
     train_iter, test_iter = data.BucketIterator.splits(
-        (train, test), batch_sizes=(50, 50), device=args.gpu, repeat=False)
+        (train, test), batch_sizes=(args.batch, args.batch), device=args.gpu, repeat=False)
 
-    model = LSTMJump(len(TEXT.vocab), 32, 128, len(LABEL.vocab), args.N, args.K, args.R)
+    model = LSTMJump(len(TEXT.vocab), 300, 128, len(LABEL.vocab), args.N, args.K, args.R)
+    model.setup_test(8, 80)
+    model.set_embedding(TEXT.vocab.vectors)
     model.cuda(args.gpu)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     max_accuracy = 0
     for i in range(1000):
@@ -42,10 +74,10 @@ def main(args):
         for batch in train_iter:
             optimizer.zero_grad()
             loss = model(batch.text, batch.label)
-            sum_loss += loss.data[0]
             loss.backward()
-            torch.nn.utils.clip_grad_norm(model.parameters(), 1)
+            torch.nn.utils.clip_grad_norm(model.parameters(), 1.)
             optimizer.step()
+            sum_loss += loss.data[0]
         print('Loss: {}'.format(sum_loss / len(train_iter)))
         sum_correct = 0
         total = 0

6ff466d9be345ef501197102d2896b6a8b459d7c
yasufumi <yasufumi.taniguchi@gmail.com>
2018-03-08 14:39:39 +0900

    update model.py

--- model.py
+++ model.py
@@ -18,11 +18,29 @@ class LSTMJump(nn.Module):
         self.mse_loss = nn.MSELoss(size_average=False)
         self.hidden_size = hidden_size
         self.N = N
+        self._N_train = N
         self.R = R
+        self._R_train = R
+
+    def set_embedding(self, tensor):
+        self.embed.weight.data.copy_(tensor)
+
+    def setup_test(self, N, R):
+        self._N_test = N
+        self._R_test = R
+
+    def train(self, mode=True):
+        self = super().train(mode)
+        self.N = self._N_train if mode else self._N_test
+        self.R = self._R_train if mode else self._R_test
+        return self
+
+    def eval(self):
+        return self.train(False)
 
     def forward(self, xs, t):
         max_length, batch_size = xs.size()
-        h = xs.data.new(1, batch_size, self.hidden_size).zero_()
+        h = Variable(xs.data.new(1, batch_size, self.hidden_size).zero_().float(), requires_grad=False)
         state = (h, h)
         embs = functional.dropout(self.embed(xs), 0.1)
         rows = xs.data.new(batch_size).zero_()
@@ -37,6 +55,8 @@ class LSTMJump(nn.Module):
                 h, state = self._maybe_lstm(embs[rows, columns][None], state,
                                             feed_previous[None, :, None].expand_as(h))
                 rows = rows + 1
+                if self._finish_reading(rows, max_length):
+                    break
             feed_previous = rows >= max_length
             # TODO: replace where function when it is added
             rows = feed_previous.long() * last_rows + (1 - feed_previous.long()) * rows
@@ -51,14 +71,49 @@ class LSTMJump(nn.Module):
             baselines.append(self.baseline(h.squeeze(0)))
             feed_previous = (jump.data == 0).long()
             rows = feed_previous * (last_rows + 1) + (1 - feed_previous) * (rows + jump.data)
+            if self._finish_reading(rows, max_length):
+                break
         y = functional.log_softmax(self.output(h.squeeze(0)), dim=1)
         reward = self._get_reward(y, t)
         baseline = torch.cat(baselines, dim=1)
         return self.nll_loss(y, t) + self._reinforce(log_probs, reward, baselines) + \
             self.mse_loss(baseline, reward.expand_as(baseline))
 
+    def inference(self, xs):
+        max_length, batch_size = xs.size()
+        h = Variable(xs.data.new(1, batch_size, self.hidden_size).zero_().float(), volatile=True)
+        state = (h, h)
+        embs = self.embed(xs)
+        rows = xs.data.new(batch_size).zero_()
+        columns = xs.data.new(range(batch_size))
+        last_rows = rows.clone().fill_(max_length - 1)
+        for _ in range(self.N):
+            for _ in range(self.R):
+                feed_previous = rows >= max_length
+                rows = feed_previous.long() * last_rows + (1 - feed_previous.long()) * rows
+                h, state = self._maybe_lstm(embs[rows, columns][None], state,
+                                            feed_previous[None, :, None].expand_as(h))
+                rows = rows + 1
+                if self._finish_reading(rows, max_length):
+                    break
+            feed_previous = (rows >= max_length)
+            # TODO: replace where function when it is added
+            rows = feed_previous.long() * last_rows + (1 - feed_previous.long()) * rows
+            h, state = self._maybe_lstm(embs[rows, columns][None], state,
+                                        feed_previous[None, :, None].expand_as(h))
+            p = functional.softmax(self.linear(h.squeeze(0)), dim=1)
+            jump = p.max(dim=1)[1]
+            feed_previous = (jump.data == 0).long()
+            rows = feed_previous * (last_rows + 1) + (1 - feed_previous) * (rows + jump.data)
+            if self._finish_reading(rows, max_length):
+                break
+        return self.output(h.squeeze(0)).max(dim=1)[1]
+
+    @staticmethod
+    def _finish_reading(rows, max_length):
+        return (rows >= max_length).all()
+
     def _maybe_lstm(self, x, state, mask):
-        print(x.size(), state[0].size(), mask.size())
         h_new, state_new = self.lstm(x, state)
         c = state_new[1]
         # filling with 0
@@ -77,28 +132,3 @@ class LSTMJump(nn.Module):
     def _get_reward(self, y, t):
         correct = y.data.max(dim=1)[1].eq(t.data).float()
         return Variable(correct.masked_fill_(correct == 0., -1), requires_grad=False).unsqueeze(1)
-
-    def inference(self, xs):
-        state = None
-        max_length, batch_size = xs.size()
-        embs = self.embed(xs)
-        rows = xs.data.new(batch_size).zero_()
-        columns = xs.data.new(range(batch_size))
-        # reading
-        length_read = self.R if self.R < max_length else max_length
-        for _ in range(length_read):
-            h, state = self.lstm(embs[rows, columns][None], state)
-            rows = rows + 1
-        # jump reading
-        last_rows = rows.clone().fill_(max_length - 1)
-        length_jump_read = self.N if self.R < max_length else 0
-        for _ in range(length_jump_read):
-            feed_previous = (rows >= max_length)
-            # TODO: replace where function when it is added
-            rows = feed_previous.long() * last_rows + (1 - feed_previous.long()) * rows
-            h, state = self._maybe_lstm(embs[rows, columns][None], state,
-                                        feed_previous[None, :, None].expand_as(h))
-            p = functional.softmax(self.linear(h.squeeze(0)), dim=1)
-            jump = p.max(dim=1)[1]
-            rows = rows + jump.data + 1
-        return self.output(h.squeeze(0)).max(dim=1)[1]
